2f74fe3028d9997acfecad63569f10b9ef436602
Keisuke Hirota <tahiro.k.ad@gmail.com>
2018-07-08 16:05:12 +0900

    すべてのファイルに対し、2to3を実施

--- assignment2/q1_classifier.py
+++ assignment2/q1_classifier.py
@@ -161,7 +161,7 @@ class SoftmaxModel(Model):
             start_time = time.time()
             average_loss = self.run_epoch(sess, inputs, labels)
             duration = time.time() - start_time
-            print 'Epoch {:}: loss = {:.2f} ({:.3f} sec)'.format(epoch, average_loss, duration)
+            print('Epoch {:}: loss = {:.2f} ({:.3f} sec)'.format(epoch, average_loss, duration))
             losses.append(average_loss)
         return losses
 
@@ -206,7 +206,7 @@ def test_softmax_model():
     # If ops are implemented correctly, the average loss should fall close to zero
     # rapidly.
     assert losses[-1] < .5
-    print "Basic (non-exhaustive) classifier tests pass"
+    print("Basic (non-exhaustive) classifier tests pass")
 
 if __name__ == "__main__":
     test_softmax_model()
